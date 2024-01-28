365752096ee2281790e10cad13088d4cac208395
Andrey Denisov <86472463+Andr-Den@users.noreply.github.com>
2024-01-28 18:42:50 +0500

    add component Main

---
+++ src/Main.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+
+class Main extends React.Component {
+  render() {
+      return (
+        <form>
+          <input />
+          <button type="submit">Поиск</button>
+        </form>
+      );
+    }
+} 
+
+export default Main;
