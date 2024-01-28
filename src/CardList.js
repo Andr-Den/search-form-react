1b5728f50dc759901097eca329b94d514d35f37c
Andrey Denisov <86472463+Andr-Den@users.noreply.github.com>
2024-01-28 18:50:46 +0500

    add component Card

--- src/CardList.js
+++ src/CardList.js
@@ -1,11 +1,11 @@
-
 import React from 'react';
+import Card from './Card';
 
 class CardList extends React.Component {
   render() {
       return (
         <div>
-
+          <Card></Card>
         </div>
       );
     }
